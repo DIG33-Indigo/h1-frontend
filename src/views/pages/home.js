cb51ba5993a75e7145ea6832e56551b501e2565f
Peta-S <ummraheil@gmail.com>
2021-07-11 18:14:32 +0300

    first commit

---
+++ src/views/pages/home.js
@@ -0,0 +1,80 @@
+import App from './../../App'
+import {html, render } from 'lit-html'
+import {gotoRoute, anchorRoute } from './../../Router'
+import Auth from './../../Auth'
+import Utils from './../../Utils'
+
+class HomeView {
+  init(){    
+    console.log('HomeView.init')
+    document.title = 'Home'    
+    this.render()    
+    Utils.pageIntroAnim()    
+  }
+
+  render(){
+    const template = html`
+      <td-app-header title="Home" user=${JSON.stringify(Auth.currentUser)}></td-app-header>
+      <div class="view-screen">
+      <body>
+        <div id = "home-container">
+        <!-- section 1 Hero image -->
+          <div id = "home-hero-section">
+            <div id = "home-hero-text">
+              <h3>Group Tours for 18 - 30 Somethings</h3>
+              <h1>Good Vibes & Safe Travels</h1>
+            </div>
+          </div>
+          
+        </div>
+
+        <!-- section 2 popular tours -->
+        <div id = "home-poptour-section">
+          <h1 id ="pop-h1">Popular Top Deck Tours:</h1>
+
+            <div class ="glider-contain multiple">
+              <!--slide in and out element-->
+              <div class = "glider">
+                <figure>
+                  <div id = "bali-adventure" class = "slide">
+                  <div class = "slider-txt">
+                  <h3>Bali Adventurer</h3>
+                    <h4>9 Nights | Departs Denpasser</h4>
+                    <div class = "slider-item"  id = "adv-slide">
+                    <span >#Adventure</span>
+                  </div>
+                </div>
+              </figure>
+
+            
+              <figure>
+                <div id = "med-reset" class = "slide">
+                  <div class = "slider-txt">
+                  <h3>Mediterranean Reset</h3>
+                    <h4>5 Nights | Departs Athens</h4>
+                    <div class = "slider-item" id = "chill-slide">
+                    <span>#Chill</span>
+                    </div>
+                    </div>
+                  </div>
+                </div>
+              </figure>   
+              </div>
+              
+              <div id ="dots" class ="glider-dots"></div>
+          </div>
+          
+        </div>
+      </body>
+      <td-site-footer></td-site-footer>
+      </div>
+
+
+
+      
+    `
+    render(template, App.rootEl)
+  }
+}
+
+export default new HomeView()
