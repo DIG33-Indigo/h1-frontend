1a05713d4fdeb8fefcb6a893e809580507fe5ce2
MiriamSaf <s3739725@student.rmit.edu.au>
2021-07-11 22:22:39 -0400

    chnage the import url so that the font is displayed correctly

---
+++ h1-frontend-main/src/views/pages/faqs.js
@@ -0,0 +1,31 @@
+import App from '../../App'
+import {html, render } from 'lit-html'
+import {gotoRoute, anchorRoute } from '../../Router'
+import Utils from '../../Utils'
+import Auth from './../../Auth'
+
+class FaqsView {
+  init(){    
+    console.log('FaqsView.init')
+    document.title = 'FAQs'    
+    this.render()    
+    Utils.pageIntroAnim()    
+  }
+
+  render(){
+    const template = html`
+     <td-app-header title="FAQs" user=${JSON.stringify(Auth.currentUser)}></td-app-header>
+      <div class="page-content">
+      
+        <h3>FAQs Content</h3>
+        
+        <td-site-footer></td-site-footer>
+      </div>
+      
+    `
+
+    render(template, App.rootEl)
+  }
+}
+
+export default new FaqsView()
