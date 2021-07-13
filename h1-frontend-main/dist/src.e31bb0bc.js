903c699f0092c322ee0c93915b7fe0396c418848
MiriamSaf <s3739725@student.rmit.edu.au>
2021-07-12 21:13:48 -0400

    continue with responsiveness for the second home section - popular tours

--- h1-frontend-main/dist/src.e31bb0bc.js
+++ h1-frontend-main/dist/src.e31bb0bc.js
@@ -14284,7 +14284,7 @@ class FaqsView {
   }
 
   render() {
-    const template = (0, _litHtml.html)(_templateObject || (_templateObject = _taggedTemplateLiteral(["\n     <td-app-header title=\"FAQs\" user=", "></td-app-header>\n      <div class=\"page-content\">\n      \n        <h3>FAQs Content</h3>\n        \n        <td-site-footer></td-site-footer>\n      </div>\n      \n    "])), JSON.stringify(_Auth.default.currentUser));
+    const template = (0, _litHtml.html)(_templateObject || (_templateObject = _taggedTemplateLiteral(["\n     <td-app-header title=\"FAQs\" user=", "></td-app-header>\n      <div class=\"page-content\">\n      \n        <h3>FAQs Content</h3>\n        <p class=\"para\"> \"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?\"\n       </p>\n        <td-site-footer></td-site-footer>\n      </div>\n      \n    "])), JSON.stringify(_Auth.default.currentUser));
     (0, _litHtml.render)(template, _App.default.rootEl);
   }
 
